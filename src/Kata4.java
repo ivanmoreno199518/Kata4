17340b05be0ef8d94b013534a74601226cf860ef
Ivan Thanon <ivanmoreno199518@gmail.com>
2016-11-07 21:14:02 +0000

    First version of Kata4

--- src/Kata4.java
+++ src/Kata4.java
@@ -1,13 +1,14 @@
 import java.io.File;
 
+
 public class Kata4 {
 
     public static void main(String[] args) {
-        File file = new File("c:\\");
+        File file = new File("/");  /* Trabajo desde Mac por eso pongo "/" */
         String[] names = file.list();
         for (String name : names) {
             System.out.println(name);
         }
     }
-    
+
 }
