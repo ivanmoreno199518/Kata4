d0bf3cf88f994a3d8352c196b0c1856408d5d73a
Ivan Thanon <ivanmoreno199518@gmail.com>
2016-11-07 21:20:01 +0000

    Second Version of Kata4

--- src/Kata4.java
+++ src/Kata4.java
@@ -1,13 +1,17 @@
 import java.io.File;
 
-
 public class Kata4 {
-
     public static void main(String[] args) {
-        File file = new File("/");  /* Trabajo desde Mac por eso pongo "/" */
-        String[] names = file.list();
-        for (String name : names) {
-            System.out.println(name);
+        File file = new File("/Users");
+        print(file.listFiles(),"");
+    }
+
+    private static void print(File[] files, String indent) {
+        if (files == null) return;
+        for (File file : files) {
+            System.out.println(indent + (file.isDirectory() ? "+" : "-") + file.getName());
+            if(!file.isDirectory() || file.isHidden()) continue;
+            print (file.listFiles(), " ");
         }
     }
 
